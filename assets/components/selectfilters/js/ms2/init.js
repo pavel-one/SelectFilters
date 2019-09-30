60aec94ee484595f61fdfa782eed5c73de8a2606
Pavel Zarubin <pavel@orendat.ru>
2019-09-30 15:46:54 +0300

    Add field window

--- assets/components/selectfilters/js/ms2/init.js
+++ assets/components/selectfilters/js/ms2/init.js
@@ -1,1 +1,10 @@
-console.log('init');
+Ext.ComponentMgr.onAvailable('minishop2-window-option-update', function () {
+    let column = this.fields[0].items[1].items;
+    column.push({
+        xtype: 'checkbox',
+        id: 'select_filter_id',
+        name: 'select_filter',
+        boxLabel: 'Показывать в фильтрах',
+        anchor: '100%'
+    });
+});
