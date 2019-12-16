7dffb4acbaf7703e82ec036b6644d1958ddff4ce
Pavel Zarubin <pavel@orendat.ru>
2019-12-16 15:48:40 +0300

    Fix checkbox

--- assets/components/selectfilters/js/ms2/init.js
+++ assets/components/selectfilters/js/ms2/init.js
@@ -1,11 +1,9 @@
 Ext.ComponentMgr.onAvailable('minishop2-window-option-update', function () {
     let column = this.fields[0].items[1].items;
     column.push({
-        xtype: 'checkbox',
+        xtype: 'xcheckbox',
         id: 'select_filter_id',
         name: 'select_filter',
-        inputValue: '1',
-        uncheckedValue: '0',
         boxLabel: 'Показывать в фильтрах',
         anchor: '100%'
     });
@@ -13,11 +11,9 @@ Ext.ComponentMgr.onAvailable('minishop2-window-option-update', function () {
 Ext.ComponentMgr.onAvailable('minishop2-window-option-create', function () {
     let column = this.fields[0].items[1].items;
     column.push({
-        xtype: 'checkbox',
+        xtype: 'xcheckbox',
         id: 'select_filter_id',
         name: 'select_filter',
-        inputValue: '1',
-        uncheckedValue: '0',
         boxLabel: 'Показывать в фильтрах',
         anchor: '100%'
     });
